f96e96ac38c8cffd8873c454d934b6feaadc75a3
varunrau <varunrau@gmail.com>
2013-04-13 19:26:41 -0700

    hface

--- api_server.py
+++ api_server.py
@@ -1,7 +1,7 @@
 import os
 from bottle import *
 import bottle
-from bottle.ext import sqlalchemy
+from bottle.ext import sqlalchemy as sqlaplugin
 from sqlalchemy import create_engine, Column, Integer, Sequence, String
 from sqlalchemy.ext.declarative import declarative_base
 import json

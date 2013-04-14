a9eeec2a15dd1cf08e9f40abd7cf6cd0ee70a96e
varunrau <varunrau@gmail.com>
2013-04-13 18:31:49 -0700

    sqlalchemy

--- api_server.py
+++ api_server.py
@@ -2,9 +2,26 @@ import os
 import bottle
 from bottle.ext import sqlalchemy
 import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
 
 app = bottle.Bottle()
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
+database_url = os.environ["DATABASE_URL"]
+
+Base = declarative_base()
+engine = create_engine(database_url, echo=True)
+
+plugin = sqlalchemy.Plugin(
+        engine,
+        Base.metadata,
+        keyword='db',
+        create=True,
+        commit=True,
+        use_kwargs=False
+)
+
+app.install(plugin)
+
 db = sqlalchemy(app)
 
 @route("/")
